46563c7edd169f8f093dee03eee07b6522b980f7
Vlad <vladfriends@yahoo.ca>
2019-04-23 19:30:18 +0200

    Turns out creating 4 circles for each square in the game (in order to visualize corners of a shape) every frame really kills performance. I commented them out for now

--- RacingGame/src/SquareGraphicsComponent.cpp
+++ RacingGame/src/SquareGraphicsComponent.cpp
@@ -18,9 +18,9 @@ void SquareGraphicsComponent::Update(const Entity& entity, sf::RenderWindow& win
 
 	for (auto corner : *(entity.GetWorldCorners()))
 	{
-		auto circle = sf::CircleShape(circleRad);
+		/*auto circle = sf::CircleShape(circleRad);
 		circle.setOrigin(circleRad, circleRad);
 		circle.setPosition(corner);
-		window.draw(circle);
+		window.draw(circle);*/
 	}
 }
