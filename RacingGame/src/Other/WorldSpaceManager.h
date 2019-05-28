6c14ae5e2e8534502f375ef7b2acc8069aa88aaf
Vlad <vladfriends@yahoo.ca>
2019-05-28 10:31:58 -0400

    Removed outdated todo

--- RacingGame/src/Other/WorldSpaceManager.h
+++ RacingGame/src/Other/WorldSpaceManager.h
@@ -21,7 +21,7 @@ public:
 
 	void AddEntityToCollisionSpace(Entity* entity);
 
-	//todo bug is backwherein if going off edge of screen, cells at top of game world are returned
+	//todo bug is back wherein if going off edge of screen, cells at top of game world are returned
 	std::vector<sf::Vector2i> GetCollisionSpaceCoords(const std::vector<sf::Vector2f>& worldCorners) const;
 	std::vector<Entity*> GetEntitiesAtCoords(const std::vector<sf::Vector2i>& coords) const;
 	void ClearWorldSpace();
